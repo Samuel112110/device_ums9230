99e607094a6b6bfd144e0debbd171e2524beb931
Samuel112110 <79588610+Samuel112110@users.noreply.github.com>
2026-01-09 01:15:40 +0100

    vendorsetup.sh

--- device/doogee/F1GC/vendorsetup.sh
+++ device/doogee/F1GC/vendorsetup.sh
@@ -5,6 +5,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-add_lunch_combo omni_F1GC-user
-add_lunch_combo omni_F1GC-userdebug
-add_lunch_combo omni_F1GC-eng
+# add_lunch_combo omni_F1GC-user
+# add_lunch_combo omni_F1GC-userdebug
+# add_lunch_combo omni_F1GC-eng
